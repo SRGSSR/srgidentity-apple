af8cfc1a77ddee00de717ea3961f7a33aa063206
Samuel Défago <defagos@gmail.com>
2018-10-08 10:55:04 +0200

    Fix subsystem

--- Framework/Sources/SRGIdentityLogger.h
+++ Framework/Sources/SRGIdentityLogger.h
@@ -9,8 +9,8 @@
 /**
  *  Helper macros for logging.
  */
-#define SRGIdentityLogVerbose(category, format, ...) SRGLogVerbose(@"ch.srgssr.analytics", category, format, ##__VA_ARGS__)
-#define SRGIdentityLogDebug(category, format, ...)   SRGLogDebug(@"ch.srgssr.analytics", category, format, ##__VA_ARGS__)
-#define SRGIdentityLogInfo(category, format, ...)    SRGLogInfo(@"ch.srgssr.analytics", category, format, ##__VA_ARGS__)
-#define SRGIdentityLogWarning(category, format, ...) SRGLogWarning(@"ch.srgssr.analytics", category, format, ##__VA_ARGS__)
-#define SRGIdentityLogError(category, format, ...)   SRGLogError(@"ch.srgssr.analytics", category, format, ##__VA_ARGS__)
+#define SRGIdentityLogVerbose(category, format, ...) SRGLogVerbose(@"ch.srgssr.identity", category, format, ##__VA_ARGS__)
+#define SRGIdentityLogDebug(category, format, ...)   SRGLogDebug(@"ch.srgssr.identity", category, format, ##__VA_ARGS__)
+#define SRGIdentityLogInfo(category, format, ...)    SRGLogInfo(@"ch.srgssr.identity", category, format, ##__VA_ARGS__)
+#define SRGIdentityLogWarning(category, format, ...) SRGLogWarning(@"ch.srgssr.identity", category, format, ##__VA_ARGS__)
+#define SRGIdentityLogError(category, format, ...)   SRGLogError(@"ch.srgssr.identity", category, format, ##__VA_ARGS__)
