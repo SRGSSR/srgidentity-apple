45abc88c9c923ef70159e6dd93bf9c0d0322afb7
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2018-10-09 21:55:27 +0200

    Add test account update

--- Tests/Sources/SRGIdentityTestCase.m
+++ Tests/Sources/SRGIdentityTestCase.m
@@ -10,7 +10,14 @@
 #import <UICKeyChainStore/UICKeyChainStore.h>
 #import <XCTest/XCTest.h>
 
+static NSURL *SRGIdentifierTestProviderURL(void)
+{
+    return [NSURL URLWithString:@"https://srgssr.local"];
+}
+
 @interface SRGIdentityTestCase : XCTestCase
+
+@property (nonatomic) NSURL *providerURL;
 
 @property (nonatomic) SRGIdentityService *identityService;
 
@@ -24,10 +31,16 @@
 
 - (void)setUp
 {
-    self.identityService = [[SRGIdentityService alloc] initWithProviderURL:[NSURL URLWithString:@"https://srgssr.local"]];
+    self.providerURL = [NSURL URLWithString:@"https://srgssr.local"];
+    
+    // Remove all items in the keychain.
+    UICKeyChainStore *keyChainStore = [UICKeyChainStore keyChainStoreWithServer:SRGIdentifierTestProviderURL() protocolType:UICKeyChainStoreProtocolTypeHTTPS];
+    [keyChainStore removeAllItems];
+    
+    self.identityService = [[SRGIdentityService alloc] initWithProviderURL:SRGIdentifierTestProviderURL()];
     
     self.loginRequestStub = [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
-        return [request.URL.host isEqual:@"srgssr.local"];
+        return [request.URL.host isEqual:SRGIdentifierTestProviderURL().host];
     } withStubResponse:^OHHTTPStubsResponse *(NSURLRequest *request) {
         if ([request.URL.path containsString:@"login"]) {
             NSURLComponents *URLComponents = [[NSURLComponents alloc] initWithURL:request.URL resolvingAgainstBaseURL:NO];
@@ -49,6 +62,18 @@
                                                statusCode:204
                                                   headers:nil] requestTime:1. responseTime:OHHTTPStubsDownloadSpeedWifi];
         }
+        else if ([request.URL.path containsString:@"profile"]) {
+            NSDictionary<NSString *, id> *account = @{ @"id" : @(1234),
+                                                       @"email" : @"test@srgssr.ch",
+                                                       @"display_name": @"Play SRG",
+                                                       @"firstname": @"Play",
+                                                       @"lastname": @"SRG",
+                                                       @"gender": @"other",
+                                                       @"date_of_birth": @"2001-01-01" };
+            return [[OHHTTPStubsResponse responseWithData:[NSJSONSerialization dataWithJSONObject:@{ @"user" : account } options:0 error:NULL]
+                                               statusCode:200
+                                                  headers:nil] requestTime:1. responseTime:OHHTTPStubsDownloadSpeedWifi];
+        }
         else {
             return [[OHHTTPStubsResponse responseWithData:[NSData data]
                                                statusCode:404
@@ -63,7 +88,7 @@
     self.identityService = nil;
     
     // Remove all items in the keychain.
-    UICKeyChainStore *keyChainStore = [UICKeyChainStore keyChainStoreWithServer:[NSURL URLWithString:@"https://srgssr.local"] protocolType:UICKeyChainStoreProtocolTypeHTTPS];
+    UICKeyChainStore *keyChainStore = [UICKeyChainStore keyChainStoreWithServer:SRGIdentifierTestProviderURL() protocolType:UICKeyChainStoreProtocolTypeHTTPS];
     [keyChainStore removeAllItems];
     
     [OHHTTPStubs removeStub:self.loginRequestStub];
@@ -83,7 +108,7 @@
         return YES;
     }];
     
-    NSURL *redirectURL = [NSURL URLWithString:@"srgidentity-tests://srgssr.local?token=0123456789"];
+    NSURL *redirectURL = [NSURL URLWithString:[NSString stringWithFormat:@"srgidentity-tests://%@?token=0123456789", SRGIdentifierTestProviderURL().host]];
     
     [self.identityService handleCallbackURL:redirectURL];
     
@@ -108,8 +133,8 @@
         return YES;
     }];
     
-    NSURL *redirectURL = [NSURL URLWithString:@"srgidentity-tests://srgssr.local?token=0123456789"];
-    
+    NSURL *redirectURL = [NSURL URLWithString:[NSString stringWithFormat:@"srgidentity-tests://%@?token=0123456789", SRGIdentifierTestProviderURL().host]];
+
     [self.identityService handleCallbackURL:redirectURL];
     
     [self waitForExpectationsWithTimeout:5. handler:nil];
@@ -132,6 +157,58 @@
     XCTAssertFalse(self.identityService.isLoggedIn);
     
     XCTAssertFalse([self.identityService logout]);
+}
+
+- (void)testAccountUpdate
+{
+    XCTAssertNil(self.identityService.emailAddress);
+    XCTAssertNil(self.identityService.sessionToken);
+    XCTAssertNil(self.identityService.account);
+    
+    XCTAssertFalse(self.identityService.isLoggedIn);
+    
+    [self expectationForNotification:SRGIdentityServiceUserDidLoginNotification object:self.identityService handler:^BOOL(NSNotification * _Nonnull notification) {
+        return YES;
+    }];
+    
+    NSURL *redirectURL = [NSURL URLWithString:[NSString stringWithFormat:@"srgidentity-tests://%@?token=0123456789", SRGIdentifierTestProviderURL().host]];
+
+    [self.identityService handleCallbackURL:redirectURL];
+    
+    [self waitForExpectationsWithTimeout:5. handler:nil];
+    
+    XCTAssertTrue(self.identityService.isLoggedIn);
+    
+    [self expectationForNotification:SRGIdentityServiceDidUpdateAccountNotification object:self.identityService handler:^BOOL(NSNotification * _Nonnull notification) {
+        XCTAssertNotNil(notification.userInfo[SRGIdentityServiceAccountKey]);
+        return YES;
+    }];
+    
+    [self waitForExpectationsWithTimeout:5. handler:nil];
+    
+    XCTAssertNotNil(self.identityService.emailAddress);
+    XCTAssertNotNil(self.identityService.sessionToken);
+    XCTAssertNotNil(self.identityService.account);
+    
+    XCTAssertTrue(self.identityService.isLoggedIn);
+    
+    [self expectationForNotification:SRGIdentityServiceUserDidLogoutNotification object:self.identityService handler:^BOOL(NSNotification * _Nonnull notification) {
+        return YES;
+    }];
+    [self expectationForNotification:SRGIdentityServiceDidUpdateAccountNotification object:self.identityService handler:^BOOL(NSNotification * _Nonnull notification) {
+        XCTAssertNil(notification.userInfo[SRGIdentityServiceAccountKey]);
+        return YES;
+    }];
+    
+    XCTAssertTrue([self.identityService logout]);
+    
+    [self waitForExpectationsWithTimeout:5. handler:nil];
+    
+    XCTAssertNil(self.identityService.emailAddress);
+    XCTAssertNil(self.identityService.sessionToken);
+    XCTAssertNil(self.identityService.account);
+    
+    XCTAssertFalse(self.identityService.isLoggedIn);
 }
 
 @end
