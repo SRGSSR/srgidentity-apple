cb72d64bf7ddc57d22f325e4dcee3f08829c870f
Samuel Défago <defagos@gmail.com>
2019-02-21 08:09:17 +0100

    Fix browser landscape layout
    This also reverts changes from commit 9874ee9406d5feb34ac08580563661a4356ea8d5.

--- Tests/TestApp/main.m
+++ Tests/TestApp/main.m
@@ -7,13 +7,14 @@
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
-@property (strong, nonatomic) UIWindow *window;
+@property (nonatomic) UIWindow *window;
 @end
 
 @implementation AppDelegate
 @end
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
