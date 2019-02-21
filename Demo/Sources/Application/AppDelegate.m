47eaae1e167bc406d8c677e76f22899f82010eef
Samuel Défago <defagos@gmail.com>
2019-02-21 09:46:12 +0100

    Display message when the user has been unauthorized

--- Demo/Sources/Application/AppDelegate.m
+++ Demo/Sources/Application/AppDelegate.m
@@ -22,9 +22,27 @@
     SRGIdentityService.currentIdentityService = [[SRGIdentityService alloc] initWithWebserviceURL:[NSURL URLWithString:@"https://hummingbird.rts.ch/api/profile"]
                                                                                        websiteURL:[NSURL URLWithString:@"https://www.rts.ch/profile"]];
     
+    [NSNotificationCenter.defaultCenter addObserver:self
+                                           selector:@selector(userDidLogout:)
+                                               name:SRGIdentityServiceUserDidLogoutNotification
+                                             object:SRGIdentityService.currentIdentityService];
+    
     DemosViewController *demosViewController = [[DemosViewController alloc] init];
     self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:demosViewController];
     return YES;
+}
+
+#pragma mark Notifications
+
+- (void)userDidLogout:(NSNotification *)notification
+{
+    if ([notification.userInfo[SRGIdentityServiceUnauthorizedKey] boolValue]) {
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:NSLocalizedString(@"Information", nil)
+                                                                                 message:NSLocalizedString(@"You have been logged out. Please login again to synchronize your data.", nil)
+                                                                          preferredStyle:UIAlertControllerStyleAlert];
+        [alertController addAction:[UIAlertAction actionWithTitle:NSLocalizedString(@"Dismiss", nil) style:UIAlertActionStyleDefault handler:nil]];
+        [self.window.rootViewController presentViewController:alertController animated:YES completion:nil];
+    }
 }
 
 @end
