4b2cbdadd707a698bff36c8b52b1f200d1cb2a23
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2020-09-25 17:15:30 +0200

    Update dependencies with SPM on main branches (except Mantle)

--- Package.swift
+++ Package.swift
@@ -20,10 +20,10 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "FXReachability", url: "https://github.com/SRGSSR/FXReachability.git", .branch("feature/spm-support")),
+        .package(name: "FXReachability", url: "https://github.com/SRGSSR/FXReachability.git", .branch("master")),
         .package(name: "Mantle", url: "https://github.com/SRGSSR/Mantle.git", .branch("swift-package-manager-support")),
-        .package(name: "SRGAppearance", url: "https://github.com/SRGSSR/srgappearance-apple.git", .branch("feature/spm-support")),
-        .package(name: "SRGNetwork", url: "https://github.com/SRGSSR/srgnetwork-apple.git", .branch("feature/spm-support")),
+        .package(name: "SRGAppearance", url: "https://github.com/SRGSSR/srgappearance-apple.git", .branch("develop")),
+        .package(name: "SRGNetwork", url: "https://github.com/SRGSSR/srgnetwork-apple.git", .branch("develop")),
         .package(name: "UICKeyChainStore", url: "https://github.com/kishikawakatsumi/UICKeyChainStore.git", .exact("2.2.0"))
     ],
     targets: [
