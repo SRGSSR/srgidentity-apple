75a2a175e6dae85cca6ec1fb22d6ed4cc79b6b5b
Samuel Défago <defagos@gmail.com>
2021-08-04 09:24:20 +0200

    Avoid instantiating a root view controller in the app delegate on iOS / tvOS 13+

--- Demo/Sources/Application/AppDelegate.h
+++ Demo/Sources/Application/AppDelegate.h
@@ -10,6 +10,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
+// TODO: Remove when SRG Analytics demo requires iOS 13
 @property (nonatomic) UIWindow *window;
 
 @end
