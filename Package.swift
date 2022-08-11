2538c844493c2c3f9f1bf76c43faacdffd828d0b
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2022-08-11 19:46:02 +0200

    Update dependencies to official tags

--- Package.swift
+++ Package.swift
@@ -20,10 +20,10 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "FXReachability", url: "https://github.com/SRGSSR/FXReachability.git", .branch("master")),
+        .package(name: "FXReachability", url: "https://github.com/SRGSSR/FXReachability.git", .exact("1.3.2-srg6")),
         .package(name: "Mantle", url: "https://github.com/Mantle/Mantle.git", .upToNextMinor(from: "2.2.0")),
-        .package(name: "SRGAppearance", url: "https://github.com/SRGSSR/srgappearance-apple.git", .branch("develop")),
-        .package(name: "SRGNetwork", url: "https://github.com/SRGSSR/srgnetwork-apple.git", .branch("develop")),
+        .package(name: "SRGAppearance", url: "https://github.com/SRGSSR/srgappearance-apple.git", .upToNextMinor(from: "5.2.0")),
+        .package(name: "SRGNetwork", url: "https://github.com/SRGSSR/srgnetwork-apple.git", .upToNextMinor(from: "3.1.0")),
         .package(name: "UICKeyChainStore", url: "https://github.com/kishikawakatsumi/UICKeyChainStore.git", .upToNextMinor(from: "2.2.1"))
     ],
     targets: [
